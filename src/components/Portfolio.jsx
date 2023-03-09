8f1d7464f49d69443ebe8a6de13d9e8a36151506
tesseir <tesseir.edu@gmail.com>
2023-03-08 20:51:37 -0600

    finished portofolio component

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -9,28 +9,28 @@ function Portfolio() {
       <div class="container">
         <div class="row">
           <div class="col">
-            <br />
+            
             <a href="https://github.com/tesseir/e-commerce"
-              target="_blank"><img src="./images/e-commerce.png" /></a>
-            <br />
+              target="_blank"><img src="./images/e-commerce.png"/></a>
+
             <a href="https://github.com/tesseir/note-taker"
-              target="_blank"><img src="./images/note-taker.png" /></a>
-            <br />
+              target="_blank"><img src="./images/note-taker.png"/></a>
+
             <a href="https://github.com/tesseir/project-tracker"
-              target="_blank"><img src="./images/project-tracker.png" /></a>
+              target="_blank"><img src="./images/project-tracker.png"/></a>
 
           </div>
           <div class="col">
-            <br />
+
             <a href="https://github.com/tesseir/readme-generator"
-              target="_blank"><img src="./images/readme-generator.png" /></a>
-            <br />
+              target="_blank"><img src="./images/readme-generator.png"/></a>
+              
             <a href="https://github.com/tesseir/social-media-api"
-              target="_blank"><img src="./images/note-taker.png" /></a>
-            <br />
+              target="_blank"><img src="./images/note-taker.png"/></a>
+
             <a href="https://github.com/tesseir/Workday-Scheduler"
-              target="_blank"><img src="./images/project-tracker.png" /></a>
-            <br/>
+              target="_blank"><img src="./images/project-tracker.png"/></a>
+
           </div>
         </div>
       </div>
